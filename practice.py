2581af1a3db05090fe7d705839bf9cfc239dfb88
16adixon <aidandixon37@gmail.com>
2023-01-30 09:28:59 -0800

    Trying to write function
    In practice.py, I attempted to write a function to ask a question. I don't know how to incorporate score with the function.
    
    Note: Not sure if anything was there, but scratch.py is empty now

--- practice.py
+++ practice.py
@@ -1,2 +1,18 @@
+questions = ["What is a candy bar"]
+correct_answers = ["a", "b", "c,", "d"]
+score = 0
+
+def ask(questions):
+    score = 0
+    answer = input(questions)
+    if answer == correct_answers[0]:
+        print("Correct!\n")
+        score += 1
+    else:
+        print("Incorrect! The correct answer was", correct_answers[0])
+
+ask(questions[0])
+
+print("Your score:", score)
 
 
--- scratch.py
+++ scratch.py
@@ -1,18 +1,2 @@
-questions = ["Do the rhcp have a star on hollywood"]
-correct_answers = ["a", "b", "c", "d"]
-
-answer = input(questions[0])
-if answer.lower() == "yes":
-    print("Correct!\n")
-    score += 1
-    answer = input(questions[0])
-    if answer == correct_answers[3]:
-        print("Correct!\n")
-        score += 1
-    else:
-        print("Incorrect! The correct answer was", correct_answers[3] + "\n")
-else:
-    print("They do have a star! It was unveiled in 2022!\n")
-
 
 
